cef218bfacfa4d6b6972706d03e80e351c059f73
Joel H <joel.hacke@gmail.com>
2020-12-11 16:32:33 -0600

    initial commit

---
+++ src/scripts/utils.js
@@ -0,0 +1,23 @@
+
+const divFactory = (...classNames) => {
+    const div = document.createElement('div')
+    if (classNames) {
+        classNames.forEach(className => {
+            div.classList.add(className)
+        });
+    }
+
+    return div
+}
+
+const createText = (content, type) => {
+    const text = document.createElement(type)
+    
+    if (content) {
+        text.innerText = content;
+    }
+
+    return text
+}
+
+export { divFactory, createText }
